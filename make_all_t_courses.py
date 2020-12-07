772a2be3e000832c822c73289fbb7f60fe7b9918
peter Quicke <peq10@ic.ac.uk>
2020-12-07 18:10:23 +0000

    HPC stuff

--- make_all_t_courses.py
+++ make_all_t_courses.py
@@ -36,9 +36,6 @@ def lab2masks(seg):
 def make_all_tc(df_file,save_dir, redo = True, njobs = 2, HPC_num = None):
     df = pd.read_csv(df_file)
     
-    if HPC_num is not None:
-        njobs = 1
-    
     if redo:
         redo_from = 0
     else:
@@ -46,43 +43,47 @@ def make_all_tc(df_file,save_dir, redo = True, njobs = 2, HPC_num = None):
         print(f'{len(df) - redo_from} to do')
 
     
-    with Parallel(n_jobs=njobs) as parallel:
-        for idx,data in enumerate(df.itertuples()):
-            if HPC_num is not None: #allows running in parallel on HPC
-                if idx != HPC_num:
-                    continue
-            
-            
-            parts = Path(data.tif_file).parts
-            trial_string = '_'.join(parts[parts.index('cancer'):-1])
-            trial_save = Path(save_dir,'ratio_stacks',trial_string)
-            
-            
-            
-            if not redo and HPC_num is None:
-                if idx < redo_from:
-                    continue
-            elif not redo and HPC_num is not None:
-                if Path(trial_save,f'{trial_string}_all_tcs.npy').is_file():
-                    continue
+
+    for idx,data in enumerate(df.itertuples()):
+        if HPC_num is not None: #allows running in parallel on HPC
+            if idx != HPC_num:
+                continue
         
-            seg = np.load(Path(trial_save,f'{trial_string}_seg.npy'))
-            
-            masks = lab2masks(seg)
-            
-            stack = np.load(Path(trial_save,f'{trial_string}_ratio_stack.npy'))
-            
+        
+        parts = Path(data.tif_file).parts
+        trial_string = '_'.join(parts[parts.index('cancer'):-1])
+        trial_save = Path(save_dir,'ratio_stacks',trial_string)
+        
+        
+        
+        if not redo and HPC_num is None:
+            if idx < redo_from:
+                continue
+        elif not redo and HPC_num is not None:
+            if Path(trial_save,f'{trial_string}_all_tcs.npy').is_file():
+                continue
+    
+        seg = np.load(Path(trial_save,f'{trial_string}_seg.npy'))
+        
+        masks = lab2masks(seg)
+        
+        stack = np.load(Path(trial_save,f'{trial_string}_ratio_stack.npy'))
+        
+        if HPC_num is None:
             try:
-                tc = parallel(delayed(t_course_from_roi)(stack,mask) for mask in masks)
+                with Parallel(n_jobs=njobs) as parallel:
+                    tc = parallel(delayed(t_course_from_roi)(stack,mask) for mask in masks)
             except Exception as err:
                 print(err)
                 tc = [t_course_from_roi(stack,mask) for mask in masks]
+        else:
+            tc = [t_course_from_roi(stack,mask) for mask in masks]
+    
+        tc = np.array(tc)
         
-            tc = np.array(tc)
-            
-            np.save(Path(trial_save,f'{trial_string}_all_tcs.npy'),tc)
-            
-            print(f'Saved {trial_string}')
-            redo_from += 1
-            np.save(Path(save_dir,f'{df_file.stem}_redo_from_make_all_tc.npy'),redo_from)
+        np.save(Path(trial_save,f'{trial_string}_all_tcs.npy'),tc)
+        
+        print(f'Saved {trial_string}')
+        redo_from += 1
+        np.save(Path(save_dir,f'{df_file.stem}_redo_from_make_all_tc.npy'),redo_from)
         
