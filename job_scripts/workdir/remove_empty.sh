8956d92b3f185c8ce9d27c012f6c18c3420d47d1
peter Quicke <peq10@ic.ac.uk>
2021-01-16 13:18:18 +0000

    refactoring

--- job_scripts/workdir/remove_empty.sh
+++
@@ -1,4 +0,0 @@
-find . -empty -exec rm "{}" \;
-echo "Number successful"
-find . -name "*.sh.o*"  -exec grep -o "Finished successfully" {} \; | wc -l
-find . -name "*.sh.o*"  -exec grep -l "Finished successfully" {} \; -exec rm "{}" \;
