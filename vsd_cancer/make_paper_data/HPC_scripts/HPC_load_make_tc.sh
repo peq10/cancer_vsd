6b11cd58f28606ae99bb3d1027a82231755ee62e
Peter Quicke <peter.quicke@crick.ac.uk>
2022-03-24 15:14:23 +0000

    Removing redo

--- vsd_cancer/make_paper_data/HPC_scripts/HPC_load_make_tc.sh
+++ vsd_cancer/make_paper_data/HPC_scripts/HPC_load_make_tc.sh
@@ -3,4 +3,4 @@
 #PBS -J 1-254
 
 
-/rds/general/user/peq10/home/anaconda3/envs/cancer_HPC/bin/python /rds/general/user/peq10/projects/thefarm2/live/Firefly/cancer/analysis/code_2022/cancer_vsd/vsd_cancer/make_paper_data/create_paper_data.py $PBS_ARRAY_INDEX 1 
+/rds/general/user/peq10/home/anaconda3/envs/cancer_HPC/bin/python /rds/general/user/peq10/projects/thefarm2/live/Firefly/cancer/analysis/code_2022/cancer_vsd/vsd_cancer/make_paper_data/create_paper_data.py $PBS_ARRAY_INDEX 0 
