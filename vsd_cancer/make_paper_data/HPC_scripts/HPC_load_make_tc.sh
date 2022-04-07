65b25ae71b27e6a22b6a7998897f2efda74d8eb1
Peter Quicke <peter.quicke@crick.ac.uk>
2022-04-07 17:36:57 +0100

    making figure and now doing extra tgfb analysis

--- vsd_cancer/make_paper_data/HPC_scripts/HPC_load_make_tc.sh
+++ vsd_cancer/make_paper_data/HPC_scripts/HPC_load_make_tc.sh
@@ -1,7 +1,5 @@
 #PBS -lwalltime=02:00:00
 #PBS -lselect=1:ncpus=1:mem=64gb
-#PBS -J 1-254
+#PBS -J 1-54
 
-LOGFILE=/rds/general/user/peq10/home/firefly_link/cancer/analysis/code_2022/cancer_vsd/vsd_cancer/make_paper_data/HPC_scripts/workdir/$PBS_ARRAY_INDEX.log
-
-/rds/general/user/peq10/home/anaconda3/envs/cancer_HPC/bin/python /rds/general/user/peq10/projects/thefarm2/live/Firefly/cancer/analysis/code_2022/cancer_vsd/vsd_cancer/make_paper_data/create_paper_data.py $PBS_ARRAY_INDEX 0 2>&1 | tee $LOGFILE
+/rds/general/user/peq10/home/anaconda3/envs/cancer_HPC/bin/python /rds/general/user/peq10/projects/thefarm2/live/Firefly/cancer/analysis/code_2022/cancer_vsd/vsd_cancer/make_paper_data/create_paper_data.py $PBS_ARRAY_INDEX 1
--- vsd_cancer/make_paper_data/create_paper_data.py
+++ vsd_cancer/make_paper_data/create_paper_data.py
@@ -60,9 +60,7 @@ if not data_dir.is_dir():
 
 
 print("Hello world")
-initial_df = Path(
-    top_dir, "analysis", "long_acqs_20220319_experiments_cell_labelled_complete.csv"
-)
+initial_df = Path(top_dir, "analysis", "long_acqs_20220407_HPC_labelled_complete.csv")
 redo_vid = False
 if HPC:
     df_ = pd.read_csv(initial_df)
@@ -70,6 +68,10 @@ if HPC:
 
     if df_.iloc[HPC_num].expt == "L468":
         redo_vid = True
+    else:
+        import sys
+
+        sys.exit()
 
 
 print("Loading tif...")
