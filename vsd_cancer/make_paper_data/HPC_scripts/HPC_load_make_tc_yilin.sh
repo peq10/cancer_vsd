bdb147e532a522c9f5c4a5614e9075ab4277c8d4
ys5320 <ys5320@ic.ac.uk>
2022-04-20 17:54:14 +0100

    Revert "checking versions"
    This reverts commit 6b9243f86dbbfc42b8ad61abc865403b2f7ea718.

--- vsd_cancer/make_paper_data/HPC_scripts/HPC_load_make_tc_yilin.sh
+++ vsd_cancer/make_paper_data/HPC_scripts/HPC_load_make_tc_yilin.sh
@@ -2,4 +2,4 @@
 #PBS -lselect=1:ncpus=4:mem=64gb
 #PBS -J 1-2
 
-/rds/general/user/ys5320/home/anaconda3/envs/cancer_HPC_tst/bin/python /rds/general/user/ys5320/projects/thefarm2/live/Firefly/cancer/analysis/code_2022/cancer_vsd/vsd_cancer/make_paper_data/create_paper_data.py $PBS_ARRAY_INDEX 1
+/rds/general/user/peq10/home/anaconda3/envs/cancer_HPC/bin/python /rds/general/user/peq10/projects/thefarm2/live/Firefly/cancer/analysis/code_2022/cancer_vsd/vsd_cancer/make_paper_data/create_paper_data.py $PBS_ARRAY_INDEX 1
